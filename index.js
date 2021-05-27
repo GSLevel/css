48d22e70abac058285748c44b54320f54068421b
GSLevel <67515314+GSLevel@users.noreply.github.com>
2021-05-27 20:25:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ setTimeout(() => {
         btns.style.opacity = "1"
     }, 8,700)
  }, 3500)
+
+
+ function getCSS() {
+    fetch('css.json')
+      .then(res => res.json())
+      .then(data => {
+         const html = data.csses.map(css => {
+            return `
+            <div id="${css.id}">
+            <h3>${css.text}</h3>
+            <img src="${css.img}" onclick="document.location.href='${css.link}'">
+            </div>
+            `
+         }).join("")
+         document.getElementById('items').insertAdjacentHTML('beforeend', html)
+      })
+ }
+
+ getCSS()
